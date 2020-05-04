247c5e4d6228ebcf0621af4bd91ede9f4319a443
lucianilie <ilielucianmarian@gmail.com>
2020-05-04 20:20:34 +0300

    chapter 2 addons

---
+++ src/main/java/chapters/02_linked_lists/_202_Return_kth_to_Last/TesterClass.java
@@ -0,0 +1,4 @@
+package _202_Return_kth_to_Last;
+
+public class TesterClass {
+}
--- src/main/java/chapters/common/ListUtilityFunctions.java
+++ src/main/java/chapters/common/ListUtilityFunctions.java
@@ -15,4 +15,14 @@ public class ListUtilityFunctions {
         }
         System.out.println();
     }
+
+    public static int getSize(Node head) {
+        Node n = head;
+        int size = 0;
+        while (n != null) {
+            size++;
+            n = n.next;
+        }
+        return size;
+    }
 }
